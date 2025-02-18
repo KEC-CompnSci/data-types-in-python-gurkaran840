98f200b221d046a8ac2a0849bc67f24da86179e7
gurkaran840 <27sidhug@retsdstudent.ca>
2025-02-18 20:33:52 +0000

    fgdh

--- src/assignment.py
+++ src/assignment.py
@@ -5,7 +5,8 @@
 num_str_1 = "42"
 num_str_2 = "13"
 num_str_3 = "7"
-word_1 = "robots"
-word_2 = "built"
-word_3 = "today"
-word_4 = "were"
+word_1 = " robots"
+word_2 = " built"
+word_3 = " today"
+word_4 = " were"
+sentence = str(int(num_str_1) + int(num_str_2)) + word_1 + word_4 + word_2 + word_3
